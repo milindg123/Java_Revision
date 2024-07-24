143cc37a4b21f698e3cbf3541083fc6b34c7840e
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-07-24 22:40:38 +0530

    commit

--- _05OOPsConcepts/src/org/ApnaCollege/Abstraction/_05StaticKeyword.java
+++ _05OOPsConcepts/src/org/ApnaCollege/Abstraction/_05StaticKeyword.java
@@ -1,9 +1,22 @@
+/*
+ 	Program to implements static keyword
+ 	
+ 	Static Keyword : static keyword in java is used to share the same variable or method of a given class
+ 	
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
 package org.ApnaCollege.Abstraction;
 
 public class _05StaticKeyword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 

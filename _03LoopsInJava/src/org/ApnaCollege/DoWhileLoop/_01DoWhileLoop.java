b76f94015c15093a247fbc624f0cf1ac62870026
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-07-26 01:43:50 +0530

    second commit

--- _03LoopsInJava/src/org/ApnaCollege/DoWhileLoop/_01DoWhileLoop.java
+++ _03LoopsInJava/src/org/ApnaCollege/DoWhileLoop/_01DoWhileLoop.java
@@ -4,18 +4,18 @@
   	 Loop 3 types
   	 
   	 1) While Loop		|	2) for loop 							|	3) do while
- 						|											|
- 	Syntax :			|	Syntax :								| 	Syntax :
- 						|											|	
- 	while(condition){	|	for(int i = 0;   i<=n;       i++ );		|	do{
- 						|	//Initialization //condition //updation	|
- 	 //do something		|											|		// Do something
- 						|											|	}
- 	}					|											|
- 						|											|	while(Condition);
+ 				|									|
+ 	Syntax :		|	Syntax :							| 	Syntax :
+ 				|									|	
+ 	while(condition){	|	for(int i = 0;   i<=n;       i++ );				|	do{
+ 				|	//Initialization //condition //updation				|
+ 	 //do something		|									|		// Do something
+ 				|									|	}
+ 	}			|									|
+ 				|									|	while(Condition);
  
  
- 		******* Do While Loop --> Execute at least once if condition is true or not 
+ 	******* Do While Loop --> Execute at least once if condition is true or not 
  
  */					 														
 

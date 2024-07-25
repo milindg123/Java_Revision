3d469d31d69ff90ff247104071b8fb0687d6722e
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-07-26 00:39:14 +0530

    Second commit

--- _05OOPsConcepts/src/org/ApnaCollege/OOPs/_08DestructorInJava.java
+++ _05OOPsConcepts/src/org/ApnaCollege/OOPs/_08DestructorInJava.java
@@ -1,11 +1,11 @@
 /*
  
- --> Destructors : 	We can not write any destructor in java there are the term called garbage collector
-  			   		which is perform the role of the destructors in java
+ --> Destructors :  We can not write any destructor in java there are the term called garbage collector
+  		    which is perform the role of the destructors in java
   			   
  
  --> Garbage Collector:  the variable, method, function which is not used in the program 
- 						 for a long time then the garbage collector automatically remove it from the program  
+ 			 for a long time then the garbage collector automatically remove it from the program  
  
  
  

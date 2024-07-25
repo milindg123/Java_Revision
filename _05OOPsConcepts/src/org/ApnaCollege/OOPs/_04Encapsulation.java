5d5b651b716167e5017b0a11b001b97b304c597e
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-07-26 00:37:00 +0530

    second commit

--- _05OOPsConcepts/src/org/ApnaCollege/OOPs/_04Encapsulation.java
+++ _05OOPsConcepts/src/org/ApnaCollege/OOPs/_04Encapsulation.java
@@ -2,11 +2,11 @@
  	Four Pillars of OOP
  	
  	1) Encapsulation :  Encapsulation is defined as the wrapping up of data and method under a single unit.
- 						It also implements data hiding.
+ 			    It also implements data hiding.
  						
  						
- 						Data Hiding : useless or sensitive data we can make them as (private) i.e private data members
- 									--> We can use access specifier for the data hiding.
+ 	   Data Hiding : useless or sensitive data we can make them as (private) i.e private data members
+ 			--> We can use access specifier for the data hiding.
  
  
  

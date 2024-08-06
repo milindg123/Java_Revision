5ccfb0b3ac35981827e2c60ada9372548011ea2f
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-08-07 00:20:01 +0530

    added

--- InterviewQuestions/src/ord/pattern/practice/InvertedHalfPyramidRotatedby180deg.java
+++ InterviewQuestions/src/ord/pattern/practice/InvertedHalfPyramidRotatedby180deg.java
@@ -1,8 +1,11 @@
+
+// Program to demonstrate the pattern of the inverted half pyramid rotated by 180 degree
+
 package ord.pattern.practice;
 
 import java.util.Scanner;
 
-public class PyramidPattern {
+public class InvertedHalfPyramidRotatedby180deg {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -10,18 +13,15 @@ public class PyramidPattern {
 		int n = sc.nextInt();
 		
 		for(int i=1; i<=n; i++) {
-			
 			for(int j=1; j<=n-i; j++) {
 				System.out.print(" ");
 			}
 			
 			for(int j=1; j<=i; j++) {
-				System.out.print("* ");
+				System.out.print("*");
 			}
 			System.out.println();
-			
 		}
-		
 
 	}
 

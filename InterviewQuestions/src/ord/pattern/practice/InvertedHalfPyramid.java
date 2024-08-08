2c7a1007e821158305a9875b2cf13d0ae9f1e082
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-08-08 23:44:19 +0530

    added

---
+++ InterviewQuestions/src/ord/pattern/practice/InvertedHalfPyramid.java
@@ -0,0 +1,21 @@
+package ord.pattern.practice;
+
+import java.util.Scanner;
+
+public class InvertedHalfPyramid {
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n : ");
+		int n = sc.nextInt();
+		for(int i=n; i>=0; i--) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}

b254cf38056693c0c39f3dadca1af4492bbd25f3
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2024-08-13 23:28:43 +0530

    added

--- InterviewQuestions/src/ord/pattern/practice/ButterflyPattern.java
+++ InterviewQuestions/src/ord/pattern/practice/ButterflyPattern.java
@@ -15,14 +15,8 @@ public class ButterflyPattern {
 			for(int j=1; j<=i; j++) {
 				System.out.print("*");
 			}
-			System.out.println();
 			
-
-		}
-		
-
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n-i; j++) {
+			for(int j=1; j<=2*(n-i); j++) {
 				System.out.print(" ");
 			}
 			
@@ -31,14 +25,27 @@ public class ButterflyPattern {
 			}
 			
 			System.out.println();
+
 		}
 		
+
 		// Reverse
 		for(int i=n; i>=1; i--) {
 			for(int j=1; j<=i; j++) {
 				System.out.print("*");
 			}
+			
+			for(int j=1; j<=2*(n-i); j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
 			System.out.println();
+			
+			
 		}
 		
 		
